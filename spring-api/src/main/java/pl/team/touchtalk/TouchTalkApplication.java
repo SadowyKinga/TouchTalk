c4bd4d6a2bba763141cf7637b2510105ffde4ff2
Jakub <stawowykuba@gmail.com>
2021-04-01 11:26:00 +0200

    second main commit

---
+++ spring-api/src/main/java/pl/team/touchtalk/TouchTalkApplication.java
@@ -0,0 +1,13 @@
+package pl.team.touchtalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouchTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouchTalkApplication.class, args);
+	}
+
+}
